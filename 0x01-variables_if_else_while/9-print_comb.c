c6f33a6594ad6dade874bc7ef69a9b109b13f16c
jen-ny-k <jenifferkaranja19@gmail.com>
2023-08-14 15:49:50 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,11 @@ int main(void)
 	for (k = 0; k < 10; k++)
 	{
 		if (k == 9)
-			putchar(k + '0'
+			putchar(k + '0');
 		putchar(k + '0');
 		putchar(',');
 		putchar(' ');
 	}
-	
 	return (0);
 
 }
