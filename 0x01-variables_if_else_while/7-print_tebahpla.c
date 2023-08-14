3838367272b14f19e4e01c057bed4a8a4d3a16fb
jen-ny-k <jenifferkaranja19@gmail.com>
2023-08-14 15:16:16 -0700

    ascending

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - my main function
+ *
+ * Return: The return ids always zero
+ */
+int main(void)
+{
+	char k;
+
+	for (k = 'z'; k >= 'a'; k--)
+		putchar(k);
+	putchar('\n');
+		return (0);
+
+}
