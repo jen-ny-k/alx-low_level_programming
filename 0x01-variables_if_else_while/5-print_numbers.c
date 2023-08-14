b67fdbb0e84399c6c8d2c9222f5c25c0b20b1429
jen-ny-k <jenifferkaranja19@gmail.com>
2023-08-14 14:42:37 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - This is my main function
+ *
+ * Return: always returns to zero
+ */
+int main(void)
+{
+	int k;
+	for (k = 0; k < 10; k++)
+		printf("%d\n", k);
+	return (0);
+
+}
