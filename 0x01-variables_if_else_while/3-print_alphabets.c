bf9e786efc3cc2c9958b1ea03d7a9b5f55bfcde2
jen-ny-k <jenifferkaranja19@gmail.com>
2023-08-14 13:20:23 -0700

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: returns zero 
+ */
+
+int main(void)
+{
+	int low = 'a';
+	int upper = 'A';
+	while (low <= 'z')
+	{
+		putchar(low);
+		low += 1;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper += 1;
+	}
+	putchar('\n');
+	return (0);
+
+}
