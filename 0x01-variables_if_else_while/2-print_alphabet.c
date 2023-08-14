5ecaedc6d8615f9c50062bab9b71b4b7acd94225
jen-ny-k <jenifferkaranja19@gmail.com>
2023-08-14 12:43:06 -0700

    Characters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: return zero
+ */
+int main(void)
+{
+	int low = 'a';
+	while (low <= 'z')
+	{
+		putchar(low);
+		low += 1;
+	}
+	putchar('\n');
+	return (0);
+
+}
