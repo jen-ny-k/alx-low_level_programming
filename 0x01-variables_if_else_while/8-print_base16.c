db7cbc7d340a952c06af040a9628a31e87ea7a5e
jen-ny-k <jenifferkaranja19@gmail.com>
2023-08-14 15:26:50 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - thos is my main function
+ *
+ * Return: Always returns zero
+ */
+int main(void)
+{
+	int j;
+	char m;
+
+	for (j = 0; j < 10; j++)
+		putchar(j + '0');
+	for (m = 'a'; m <= 'f' m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+
+}
