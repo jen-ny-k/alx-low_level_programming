040ddd906ee711ae2733c43415ecc74f5df54611
jen-ny-k <jenifferkaranja19@gmail.com>
2023-08-15 15:24:27 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - This is my main functionn
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+	return (0);
+
+}
