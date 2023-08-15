aba7d0b7b4829c19dd9dd6e453ce1f51290684c0
jen-ny-k <jenifferkaranja19@gmail.com>
2023-08-15 15:58:03 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include "main.h"
 /**
  * main - This is my main functionn
  *
@@ -11,9 +11,9 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		putchar(c[i]);
+		_putchar(c[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 
 }
