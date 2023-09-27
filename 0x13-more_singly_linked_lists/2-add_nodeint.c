ba74434a06f68f33460e5d3df99a61eafc1d604e
je-ny-k <jenifferkaranja19@gmail.com>
2023-09-26 17:48:09 -0700

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_nodeint - function to add node at the first place
+ * @head: first node
+ * @n: number of elmets in the link
+ * Return: 0
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	if (head == NULL)
+		return (0);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+	new_node->n = n;
+	*head = new_node;
+	return (0);
+}
